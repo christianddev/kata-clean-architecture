1cd7153a61a87b7c0b76359bfa8f28f9442a7515
Paco Rodriguez <francisco.rodriguez@icareweb.com>
2023-11-14 23:15:43 +0100

    add tests for createuser and get user cases

--- __tests__/domain/useCases/AddUser.test.ts
+++
@@ -1,5 +0,0 @@
-describe('Tests on AddUserUseCase', () => {
-    it('should be true', () => {
-        expect(true).toBe(true);
-    })
-});
--- __tests__/domain/useCases/GetUsers.test.ts
+++ __tests__/domain/useCases/GetUsers.test.ts
@@ -1,5 +1,53 @@
-describe('Tests on GetUsersUseCase', () => {
-    it('should be true', () => {
-        expect(true).toBe(true);
-    })
+import { User } from "../../../domain/entities/User";
+import { GetUsersUseCase } from "../../../domain/useCases/GetUsers";
+
+
+// Mock User and UserRepository for testing purposes
+const mockUser1 = User.create({
+  id: '123',
+  name: 'Test User',
+  email: 'test@example.com',
+  password: 'password123',
+});
+
+const mockUser2 = User.create({
+  id: '321',
+  name: 'Test User 2',
+  email: 'test2@example.com',
+  password: 'password123',
+});
+
+const mockUsers = [mockUser1, mockUser2];
+
+const mockUserRepository = {
+  getUsers: jest.fn(),
+  createUser: jest.fn(),
+  getUserByEmail: jest.fn(),
+};
+
+describe('GetUsersUseCase', () => {
+  let getUsersUseCase: GetUsersUseCase;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    getUsersUseCase = new GetUsersUseCase(mockUserRepository);
+  });
+
+  it('should retrieve users successfully', async () => {
+    mockUserRepository.getUsers.mockResolvedValue(mockUsers);
+
+    const result = await getUsersUseCase.run();
+
+    expect(mockUserRepository.getUsers).toHaveBeenCalled();
+    expect(result).toEqual(mockUsers);
+  });
+
+  it('should handle repository error', async () => {
+    mockUserRepository.getUsers.mockRejectedValue(new Error('Repository error'));
+
+    await expect(getUsersUseCase.run()).rejects.toThrow('Repository error');
+
+    expect(mockUserRepository.getUsers).toHaveBeenCalled();
+  });
 });
